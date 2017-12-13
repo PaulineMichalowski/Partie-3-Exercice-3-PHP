930467bb9e24c9ced1501c02eb5bc435d9789cc5
Pauline Michalowski <michalowski.pauline@hotmail.fr>
2017-12-13 10:13:12 +0100

    Création de l'exercice 3

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 3</title>
+  </head>
+  <body>
+    <?php
+      $number1 = 100;
+      $number2 = 12;
+      while($number1 >= 10) {
+        echo ' ' .$number1 * $number2;
+        $number1--;
+      }
+    ?>
+  </body>
+</html>
